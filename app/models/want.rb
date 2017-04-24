2433719ed55e572bdb6eb85b0cf711a18acd5701
YuukiShimoda <shimoyuki.mk2@gmail.com>
2017-04-24 07:20:36 +0000

    want

---
+++ app/models/want.rb
@@ -0,0 +1,14 @@
+class Want < Ownership
+	def want(item)
+    self.wants.find_or_create_by(item_id: item.id)
+  end
+
+  def unwant(item)
+    want = self.wants.find_by(item_id: item.id)
+    want.destroy if want
+  end
+
+  def want?(item)
+    self.want_items.include?(item)
+  end
+end
