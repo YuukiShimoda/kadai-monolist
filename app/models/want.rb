099c18c47437a449222ea51d1a87659c2c41258a
YuukiShimoda <shimoyuki.mk2@gmail.com>
2017-04-25 02:34:45 +0000

    ranking

--- app/models/want.rb
+++ app/models/want.rb
@@ -1,5 +1,5 @@
 class Want < Ownership
-	def want(item)
+  def want(item)
     self.wants.find_or_create_by(item_id: item.id)
   end
 
@@ -10,5 +10,9 @@ class Want < Ownership
 
   def want?(item)
     self.want_items.include?(item)
+  end
+  
+  def self.ranking
+    self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
   end
 end
